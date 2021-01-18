b5497da002d4ac2c150d3dc51dc43246534855cb
mkmkkim <74914390+mkmkkim@users.noreply.github.com>
2021-01-19 01:26:22 +0900

    Add files via upload

---
+++ 부분집합 검사 알고리즘/부분집합 검사 알고리즘.cpp
@@ -0,0 +1,50 @@
+﻿ #include<iostream>
+using namespace std;
+int main() {
+	int i, j, na, nb, num = 0;
+	int count = 0;
+	cout << "The number of A is:" << ' ';
+	cin >> na;
+	cout << "The number of B is:" << ' ';
+	cin >> nb;
+	if (na <= 0 || nb <= 0) return 0;
+	int* A = new int[na];
+	int* B = new int[nb];
+	for (i = 0; i < na; i++) {
+		cout << "Input the A element" << ' ';
+		cin >> A[i];
+	}
+	for (i = 0; i < nb; i++) {
+		cout << "Input the B element" << ' ';
+		cin >> B[i];
+	}
+	if (na < nb) {
+		for (i = 0; i < na; i++) {
+			for (j = 0; j < nb; j++) {
+				if (A[i] == B[j]) count++;
+			}
+		}
+		if (count == na) cout << "A is B's subset" << endl;
+		else cout << "NONE" << endl;
+	}
+	else if (na > nb) {
+		for (i = 0; i < nb; i++) {
+			for (j = 0; j < na; j++) {
+				if (A[j] == B[i]) count++;
+			}
+		}
+		if (count == nb) cout << "B is A's subset" << endl;
+		else cout << "NONE" << endl;
+	}
+	else {
+		for (i = 0; i < nb; i++) {
+			for (j = 0; j < na; j++) {
+				if (A[j] == B[i]) count++;
+			}
+			if (count == na) cout << "A=B" << endl;
+			else cout << "NONE" << endl;
+		}
+	}
+	delete[] A;
+	delete[] B;
+}
---
+++ 포인터와 참조 연습/포인터와 참조 연습.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+using namespace std;
+class Monster {
+public:
+	int HP = 100;
+	int Damage = 20;
+	char* name = "주황 버섯";
+};
+struct Player {
+	int HP = 100;
+	int Damage = 20;
+	char* name = "엔젤릭버스터";
+};
+int main() {
+	Player player;
+	Player* player2 = new Player;
+	player2->Damage = 10;
+
+	Monster monster;
+	monster.Damage = 50;
+
+	Monster* monster2 = new Monster();
+	monster2->HP = 90;
+	cout << player.name <<"의 데미지:" << player.Damage<<endl;
+	cout << "플레이어2의 데미지:" << player2->Damage << endl;
+	cout << monster.name <<"의 데미지:" << monster.Damage << endl;
+	cout << "몬스터의 체력:" << monster2->HP << endl;
+}
